5776900e975e573e41e823d7887a839992beac77
Jocelyn1912 <toty_2410@hotmail.com>
2018-02-07 12:34:50 -0300

    Agregando destino autocompletado y trazado de ruta

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,117 +1,93 @@
-//Evento API google maps
-google.maps.event.addDomListener(window, "load", function(){ // Objeto, evento y función.
-  //Objeto de clase
-  const ubicacion = new Localizacion(()=>{
-    const myLatLng = {
-      lat: ubicacion.latitude,
-      lng: ubicacion.longitude
-    };
-    //Ventana info
-    const options = {
-      center: myLatLng,
-      zoom: 16
-    }
-    const map = document.getElementById('map');
-    const mapa = new google.maps.Map(map, options);
-    
-    //Marcador
-    const marcador = new google.maps.Marker({
-      position: myLatLng,
-      map: mapa
-    });
 
-    //Mostrar ventana info
-    const informacion = new google.maps.InfoWindow();
-    //Evento del marcador
-    marcador.addListener('click', function(){
-      informacion.open(mapa, marcador);
-    });
-
-    //Input autocompletable
-    const autocomplete = document.getElementById('origen');
-    //Creando objeto auto completado
-    const search = new google.maps.places.Autocomplete(autocomplete);
-    //Enlazando al mapa
-    search.bindTo("bounds", mapa); // bounds restringe busquedas
-    search.addListener('place_changed', function(){
-      //Cerrando info window
-      informacion.close();
-      //Ocultar marcador
-      marcador.setVisible(false);
-
-      //Guardando el nuevo lugar, obteniendo busqueda
-      var place = search.getPlace();
-      //Si el lugar no se puede mostrar
-      if (!place.geometry.viewport) { //geometry propiedad nos permite hacer calculos sobre la superficie
-        window.alert("Error al mostrar el lugar");
-        return;
-      }
-      //Si el lugar se puede mostrar
-      if (place.geometry.viewport) {
-        mapa.fitBounds(place.geometry.viewport);
-      } else{
-        mapa.setCenter(place.geometry.location);
-        mapa.setzoom(18);
-      }
-
-      marcador.setPosition(place.geometry.location);
-      marcador.setVisible(true);
-
-      //
-      var addres = "";
-      if (place.address_components) {
-        //Obteniendo cada componente de la dirección
-        address = [
-        (place.address_components[0] && place.address_components[0].short_name || '' ),
-        (place.address_components[1] && place.address_components[1].short_name || '' ),
-        (place.address_components[2] && place.address_components[2].short_name || '' )
-        ];
-      }
-      informacion.setContent('<div><strong>' + place.name + '</strong><br>' + address);
-      informacion.open(map,marcador);
-    });
-  });
+function initMap() {
+  var map = new google.maps.Map(document.getElementById('map'), {
+  mapTypeControl: false,
+  center: {lat: -33.418878299999996, lng: -70.64229580000001},
+  zoom: 13
 });
 
+new AutocompleteDirectionsHandler(map);
+}
 
+/**
+* @constructor
+*/
+function AutocompleteDirectionsHandler(map) {
+  this.map = map;
+  this.originPlaceId = null;
+  this.destinationPlaceId = null;
+  this.travelMode = 'WALKING';
+  var originInput = document.getElementById('origen');
+  var destinationInput = document.getElementById('destino');
+  /*var modeSelector = document.getElementById('mode-selector');*/
+  this.directionsService = new google.maps.DirectionsService;
+  this.directionsDisplay = new google.maps.DirectionsRenderer;
+  this.directionsDisplay.setMap(map);
 
+  var originAutocomplete = new google.maps.places.Autocomplete(
+  originInput, {placeIdOnly: true});
+  var destinationAutocomplete = new google.maps.places.Autocomplete(
+  destinationInput, {placeIdOnly: true});
 /*
-function findMe() {
+  this.setupClickListener('changemode-walking', 'WALKING');
+  this.setupClickListener('changemode-transit', 'TRANSIT');
+  this.setupClickListener('changemode-driving', 'DRIVING');
+*/
+  this.setupPlaceChangedListener(originAutocomplete, 'ORIG');
+  this.setupPlaceChangedListener(destinationAutocomplete, 'DEST');
+/*
+  this.map.controls[google.maps.ControlPosition.TOP_LEFT].push(originInput);
+  this.map.controls[google.maps.ControlPosition.TOP_LEFT].push(destinationInput);
+  this.map.controls[google.maps.ControlPosition.TOP_LEFT].push(modeSelector);
+  */
+}
 
-    var output = document.getElementById('mapa');
-   
-    // Obtenemos latitud y longitud
-    function localizacion(posicion) {
-      var latitud = posicion.coords.latitude;
-      var longitud = posicion.coords.longitude;
-      var imgURL = "https://maps.googleapis.com/maps/api/staticmap?center=" + latitud + "," + longitud + "&size=600x300&markers=color:red%7C" + latitud + "," + longitud + "&key=AIzaSyD9URB1vdsmZT87QSzXoY28uPXALN44I1o";
-      output.innerHTML = "<img src=" + imgURL + ">"
-
-      //output.innerHTML = "<p>Latitud: " + latitud + "<br>Longitud: " + longitud + "</p>";
-    }
-
-    function error() {
-      output.innerHTML = "<p>No se pudo obtener tu ubicación</p>";
-    }
-
-    
-    navigator.geolocation.getCurrentPosition(localizacion, error);
-onclick="findMe()"
+// Sets a listener on a radio button to change the filter type on Places
+// Autocomplete.
+/*
+AutocompleteDirectionsHandler.prototype.setupClickListener = function(id, mode) {
+  var radioButton = document.getElementById(id);
+  var me = this;
+  radioButton.addEventListener('click', function() {
+    me.travelMode = mode;
+    me.route();
+  });
 };
 */
-/*
-function initMap() {
-  var laboratoria = {lat: -33.45, lng: -70.6667};
-  var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 16,
-    center: laboratoria
-  });
-  var marker = new google.maps.Marker({ //marcador rojo
-    position: laboratoria,
-    map: map
+
+
+AutocompleteDirectionsHandler.prototype.setupPlaceChangedListener = function(autocomplete, mode) {
+  var me = this;
+  autocomplete.bindTo('bounds', this.map);
+  autocomplete.addListener('place_changed', function() {
+    var place = autocomplete.getPlace();
+    if (!place.place_id) {
+      window.alert("Please select an option from the dropdown list.");
+      return;
+    }
+    if (mode === 'ORIG') {
+      me.originPlaceId = place.place_id;
+    } else {
+      me.destinationPlaceId = place.place_id;
+    }
+    me.route();
   });
 };
-*/
 
-
-
+AutocompleteDirectionsHandler.prototype.route = function() {
+  if (!this.originPlaceId || !this.destinationPlaceId) {
+    return;
+  }
+  var me = this;
+  this.directionsService.route({
+    origin: {'placeId': this.originPlaceId},
+    destination: {'placeId': this.destinationPlaceId},
+    travelMode: this.travelMode
+  }, function(response, status) {
+    if (status === 'OK') {
+      me.directionsDisplay.setDirections(response);
+    } else {
+      window.alert('Directions request failed due to ' + status);
+    }
+  });
+};
